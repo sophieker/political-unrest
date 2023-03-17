89e9b39b436bb7caf21eb422467fc70674a9c431
Ethan Yun <ethanyun@ohs.stanford.edu>
2023-03-17 11:12:20 -0700

    Better scraper

--- src/twitter_scrape.py
+++ src/twitter_scrape.py
@@ -8,9 +8,22 @@ TWITTER_BEARER = os.environ.get("TWITTER_BEARER")
 
 api = tweepy.Client(TWITTER_BEARER)
 
-tweets = api.search_recent_tweets("#KanyeWest", max_results=100)
+tweets_unrest1 = api.search_recent_tweets("#ukraine", max_results=100)
+tweets_unrest2 = api.search_recent_tweets("manipulative algorithm", max_results=100)
+tweets_unrest3 = api.search_recent_tweets("#TrumpCrimeFamily", max_results=100)
+tweets_rest1 = api.search_recent_tweets("Halle", max_results=100)
+tweets_rest2 = api.search_recent_tweets("Nadal", max_results=100)
+tweets_rest3 = api.search_recent_tweets("lebron james", max_results=100)
 
-tweets = [tweet.data for tweet in tweets.data]
+tweets_unrest = [tweet.data for tweet in tweets_unrest1.data]
+tweets_unrest += [tweet.data for tweet in tweets_unrest2.data]
+tweets_unrest += [tweet.data for tweet in tweets_unrest3.data]
+tweets_rest = [tweet.data for tweet in tweets_rest1.data]
+tweets_rest += [tweet.data for tweet in tweets_rest2.data]
+tweets_rest += [tweet.data for tweet in tweets_rest3.data]
 
-with open("data.json", "w") as f:
-	json.dump(tweets, f)
+with open("unrest_data.json", "w") as f:
+	json.dump(tweets_unrest, f)
+
+with open("rest_data.json", "w") as f:
+	json.dump(tweets_rest, f)
